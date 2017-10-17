63da5d91d6577a7429081eb43d312e82cf95ccda
WhiteLotus08 <32683329+WhiteLotus08@users.noreply.github.com>
2017-10-17 18:32:27 -0500

    PrimerUpload
    upload de codigo para hacer pruebas

---
+++ Vacation 360/Vacation 360-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Vacation 360-Bridging-Header.h
+//  Vacation 360
+//
+//  Created by erick on 10/17/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+//#ifndef Vacation_360_Bridging_Header_h
+//#define Vacation_360_Bridging_Header_h
+
+#import "GVRPanoramaView.h"
+#import "GVRWidgetView.h"
+#import "GVRVideoView.h"
+
